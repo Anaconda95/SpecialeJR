78894dd9c383322fb36785a319348709a76231c9
smoer40 <61831964+smoer40@users.noreply.github.com>
2021-04-21 16:27:25 +0200

    kjjk

--- Prisdata DST/aggregering.R
+++ Prisdata DST/aggregering.R
@@ -3,77 +3,322 @@ rm(list=ls())
 
 library("readxl")
 library("dplyr")
+library("rlang")
 
 options(scipen=999)
 fu02 <- read_xlsx("fu02_loebpris.xlsx")
 df <- read_xlsx("decildata.xlsx")
 
-#FØDEVARER OG IKKE-ALKOHOL
-varegrupper = list(1, 1111,1112,1113,1114,1115,1116,1117,1118,
-                   1121,1122,1123,1124,1125,1126,1127,1128,
-                   1131,1132,1133,1134,1135,1136,
-                   1141,1142,1143,1144,1145,1146,1147,
-                   1151,1152,1153,1154,1155,
-                   1161,1162,1163,1164,
-                   1171,1172,1173,1174,1175,1176,
-                   1181,1182,1183,1184,1185,1186,
-                   1191,1192,1193,1194,1199,
-                   1211,1212,1213,1221,1222,1223)
+# DEFINERER VAREGRUPPER ---------------------------------
+  varegrupper = list(
+    "01   ",      #FØDEVARER OG IKKE-ALKOHOLISKE DRIKKEVARER
+    "01111",      #Ris
+    "01112",      #Mel og gryn
+    "01113",      #Brød
+    "01114",      #Andet bagværk
+    "01115",      #Pizza og quiche
+    "01116",      #Pastaprodukter og couscous
+    "01117",      #Morgenmadsprodukter
+    "01118",      #Andre kornprodukter
+    "01121",      #Okse- og kalvekød
+    "01122",      #Svinekød
+    "01123",      #Lamme- og gedekød
+    "01124",      #Fjerkræ
+    "01125",      #Andet kød
+    "01126",      #Indmad ol
+    "01127",      #Kød, tørret, saltet eller røget
+    "01128",      #Andre tilberedninger af kød
+    "01131",      #Fisk, fersk
+    "01132",      #Fisk, frossen
+    "01133",      #Skaldyr, fersk
+    "01134",      #Skaldyr, frossen
+    "01135",      #Fisk og skaldyr, tørrede, røgede eller saltede
+    "01136",      #Fisk og skaldyr, konservede og forarbejdede
+    "01141",      #Mælk, frisk
+    "01142",      #Mælk med lavt fedtindhold, frisk
+    "01143",      #Mælk, konserveret
+    "01144",      #Yoghurt
+    "01145",      #Ost
+    "01146",      #Andre mejeriprodukter
+    "01147",      #Æg
+    "01151",      #Smør
+    "01152",      #Margarine og andre vegetabilske fedtstoffer
+    "01153",      #Olivenolie
+    "01154",      #Andre spiselige olier
+    "01155",      #Andre spiselige animalske fedtstoffer
+    "01161",      #Frugt, frisk
+    "01162",      #Frugt, frossen
+    "01163",      #Frugter og nødder, tørrede
+    "01164",      #Frugter og frugtbaserede produkter, konservede
+    "01171",      #Grøntsager ekskl kartofler, frisk
+    "01172",      #Grøntsager, frosne
+    "01173",      #Grøntsager, tørrede og forarbejdede
+    "01174",      #Kartofler og kartoffelprodukter
+    "01175",      #Chips mv
+    "01176",      #Andre rod-og knoldgrøntsager
+    "01181",      #Sukker ol
+    "01182",      #Syltetøj, marmelade og honning
+    "01183",      #Chokolade
+    "01184",      #Slik og marcipan
+    "01185",      #Konsumis
+    "01186",      #Kunstige sukkererstatninger
+    "01191",      #Sovs, smagspræparater
+    "01192",      #Salt, krydderier og køkkenurter
+    "01193",      #Babymad
+    "01194",      #Færdigretter
+    "01199",      #Andre fødevarer
+    "01211",      #Kaffe
+    "01212",      #Te
+    "01213",      #Kakao og chokoladepulver
+    "01221",      #Mineralvand eller kildevand
+    "01222",      #Læskedrikke
+    "01223",      #Frugt- og grøntsagssaft samt koncentrater til saft
+    "02   ",      #ALKOHOLISKE DRIKKEVARER OG TOBAK
+    "02111",      #Spiritus og likør
+    "02112",      #Alkoholiske læskedrikke
+    "02121",      #Vin af druer
+    "02122",      #Vin af andre frugter
+    "02123",      #Hedvin
+    "02124",      #Vinbaserede drikkevarer og alkoholfri vin
+    "02131",      #Pilsnerøl, guldøl
+    "02132",      #Andre alkoholholdige øl
+    "02133",      #Øl med lavt alkoholindhold og alkoholfri øl
+    "02134",      #Øl-baserede drikkevarer
+    "02201",      #Cigaretter
+    "02202",      #Cigarer ol
+    "02203",      #Andre tobaksvarer
+    "02300",      #Euforiserende stoffer
+    "03   ",      #BEKLÆDNING OG FODTØJ
+    "03110",      #Materialer til beklædning
+    "03121",      #Beklædning til mænd
+    "03122",      #Beklædning til kvinder
+    "03123",      #Beklædning til småbørn (0-2 år) og børn (3-13 år)
+    "03131",      #Andre artikler til beklædning
+    "03132",      #Beklædningstilbehør
+    "03141",      #Vask og rensning af tøj
+    "03142",      #Reparation og leje af beklædning
+    "03211",      #Fodtøj til mænd
+    "03212",      #Fodtøj til kvinder
+    "03213",      #Fodtøj til børn under 14 år
+    "03220",      #Reparation af fodtøj
+    "04   ",      #BOLIGBENYTTELSE, ELEKTRICITET OG OPVARMNING
+    "04110",      #Faktisk husleje betalt af lejere
+    "04121",      #Lejernes faktiske husleje for fritidsbolig
+    "04210",      #Beregnet lejeværdi af egen bolig
+    "04220",      #Andre beregnede lejeværdier
+    "04310",      #Materialer til vedligeholdelse og reparation af bolig
+    "04321",      #Tjenesteydelser, blikkenslagere
+    "04322",      #Tjenesteydelser, elektrikere
+    "04323",      #Vedligeholdelse af varmeanlæg
+    "04324",      #Tjenesteydelser, malere
+    "04325",      #Tjenesteydelser, tømrere og snedkere
+    "04329",      #Andre tjenester til vedligeholdelse og reparation af bolig
+    "04410",      #Vandforsyning
+    "04420",      #Renovation
+    "04430",      #Vandafledningsafgift mv
+    "04442",      #Sikkerhedstjenester
+    "04449",      #Andre tjenester i forbindelse med boliger
+    "04510",      #Elektricitet
+    "04521",      #Natur- og bygas
+    "04522",      #Flydende kulbrinter
+    "04530",      #Flydende brændsel
+    "04541",      #Kul
+    "04549",      #Andet fast brændsel
+    "04550",      #Fjernvarme mv
+    "05   ",      #MØBLER, HUSHOLDNINGSUDSTYR OG HUSHOLDNINGSTJENESTER
+    "05111",      #Boligmøbler
+    "05112",      #Havemøbler
+    "05113",      #Belysningsudstyr
+    "05119",      #Andre møbler og  andet boligudstyr
+    "05121",      #Gulvtæpper
+    "05122",      #Anden gulvbelægning
+    "05123",      #Montering af faste tæpper og gulvbelægning
+    "05130",      #Reparation af møbler, boligudstyr og gulvbelægninger
+    "05201",      #Møbelstof og gardiner
+    "05202",      #Sengelinned
+    "05203",      #Duge og håndklæder
+    "05204",      #Reparation af boligtekstiler
+    "05209",      #Andre boligtekstiler
+    "05311",      #Køleskabe, frysere og køle-fryseskabe
+    "05312",      #Vaskemaskiner, tørretumlere og opvaskemaskiner
+    "05313",      #Komfurer
+    "05314",      #Varmeapparater, klimaanlæg
+    "05315",      #Rengøringsudstyr
+    "05319",      #Andre større husholdningsapparater
+    "05321",      #Foodprocessorer
+    "05322",      #Kaffemaskiner, elkedler ol
+    "05323",      #Strygejern
+    "05324",      #Toaster, brødrister ol
+    "05329",      #Andre mindre elektriske husholdningsapparater
+    "05330",      #Reparation og leje af husholdningsmaskiner
+    "05401",      #Glas, keramik og porcelæn
+    "05402",      #Spisebestik, fade og sølvtøj
+    "05403",      #Ikke-elektriske køkkenredskaber og artikler
+    "05511",      #Motoriserede større redskaber og udstyr
+    "05512",      #Reparation og leje af udstyr og større redskaber
+    "05521",      #Ikke-motoriserede mindre redskaber
+    "05522",      #Diverse tilbehør til mindre redskaber
+    "05523",      #Reparation af ikke-motoriserede mindre redskaber, tilbehør
+    "05611",      #Rengørings- og vedligeholdelsesprodukter
+    "05612",      #Andre ikke-varige mindre husholdningsartikler
+    "05621",      #Hjælp til hjem og have
+    "05623",      #Leje af møbler og boligudstyr
+    "05629",      #Rengøring og vinduespudsning
+    "06   ",      #SUNDHED
+    "06110",      #Farmaceutiske produkter
+    "06121",      #Graviditetstest, kondomer mv
+    "06129",      #Andre medicinske produkter ol
+    "06131",      #Briller med korrigerende glas og kontaktlinser
+    "06132",      #Høreapparater
+    "06133",      #Reparation af terapeutiske apparater og udstyr
+    "06139",      #Andre terapeutiske apparater
+    "06211",      #Behandling hos praktiserende læge
+    "06212",      #Behandling hos speciallæge
+    "06220",      #Tandbehandling
+    "06231",      #Tjenesteydelser, laboratorier og røntgenklinikker
+    "06239",      #Andre paramedicinske tjenesteydelser
+    "06300",      #Hospitalstjenester
+    "07   ",      #TRANSPORT
+    "07111",      #Nye biler
+    "07112",      #Brugte biler
+    "07120",      #Motorcykler
+    "07130",      #Cykler
+    "07211",      #Dæk
+    "07212",      #Reservedele til personlige transportmidler
+    "07213",      #Tilbehør til personlige transportmidler
+    "07221",      #Diesel
+    "07222",      #Benzin
+    "07223",      #Andre brændstoffer til personlige transportmidler
+    "07224",      #Smøremidler
+    "07230",      #Vedligeholdelse og reparation af personlige transportmidler
+    "07241",      #Leje af garage, fast parkering og personlige transportmidler
+    "07242",      #Vejafgifter og parkeringsafgift
+    "07243",      #Køretimer, kørekort og syn af bil
+    "07311",      #Personbefordring med tog
+    "07312",      #Personbefordring med metro
+    "07321",      #Personbefordring med bus
+    "07322",      #Personbefordring med taxi og lejet bil med fører
+    "07331",      #Indenrigsflyvning
+    "07332",      #International flyvning
+    "07341",      #Personbefordring ad søvejen
+    "07350",      #Kombineret personbefordring
+    "07362",      #Flytning og opmagasinering
+    "07369",      #Andre købte transporttjenester ol
+    "08   ",      #KOMMUNIKATION
+    "08101",      #Posttjenester
+    "08201",      #Fastnettelefon og udstyr
+    "08202",      #Mobiltelefon og udstyr
+    "08204",      #Reparation af telefon- og telefaxudstyr
+    "08301",      #Fastnettelefon, tjenester
+    "08302",      #Mobiltelefon, tjenester
+    "08303",      #Internet
+    "08304",      #Kombinerede telekommunikationstjenester
+    "09   ",      #FRITID OG KULTUR
+    "09111",      #Musikanlæg, højttalere ol
+    "09112",      #Tv, dvd-afspiller, videooptager ol
+    "09119",      #Høretelefoner, digitale fotorammer, E-bogslæsere ol
+    "09121",      #Kameraer
+    "09123",      #Optiske instrumenter
+    "09131",      #PCer, tablets mv
+    "09132",      #Tilbehør til databehandlingsudstyr
+    "09133",      #Software
+    "09134",      #Regnemaskiner og andet databehandlingsudstyr
+    "09141",      #Indspillede optagemedier
+    "09142",      #Uindspillede optagemedier
+    "09149",      #Andre optagemedier
+    "09150",      #Reparation af audiovisuelt og fotografisk udstyr mv
+    "09211",      #Autocampere, campingvogne og påhængsvogne
+    "09213",      #Både, påhængsmotorer og montering af udstyr i både
+    "09214",      #Heste og tilbehør
+    "09215",      #Udstyr til spil og sport
+    "09221",      #Musikinstrumenter
+    "09230",      #Vedligeholdelse og reparation af andre større forbrugsgoder
+    "09311",      #Spil og hobbyartikler
+    "09312",      #Legetøj og festartikler
+    "09321",      #Udstyr til sport
+    "09322",      #Udstyr til camping og friluftsaktiviteter
+    "09323",      #Reparation af udstyr til sport, camping og friluftsaktivitet
+    "09331",      #Haveprodukter
+    "09332",      #Planter og blomster
+    "09341",      #Køb af kæledyr
+    "09342",      #Artikler til kæledyr
+    "09350",      #Dyrlæge og andre tjenester i forbindelse med kæledyr
+    "09411",      #Fritids- og sportstjenester - tilstedeværelse
+    "09412",      #Fritids- og sportstjenester - deltagelse
+    "09421",      #Biografer, teatre, koncerter
+    "09422",      #Museer, zoologiske haver mv
+    "09423",      #Tv og radiolicens, abonnementer
+    "09424",      #Leje af udstyr og tilbehør i forbindelse med kultur
+    "09425",      #Fotografiske tjenester
+    "09429",      #Andre kulturtjenester
+    "09430",      #Hasardspil
+    "09511",      #Skønliteratur
+    "09512",      #Undervisningsbøger
+    "09513",      #Anden faglitteratur
+    "09514",      #E-bøger mv
+    "09521",      #Aviser
+    "09522",      #Blade og tidsskrifter
+    "09530",      #Diverse tryksager
+    "09541",      #Papirprodukter
+    "09549",      #Andre papirvarer og tegnematerialer
+    "09601",      #Indenlandske pakkerejser
+    "09602",      #Udenlandske pakkerejser
+    "10   ",      #UDDANNELSE
+    "10102",      #Grundskole
+    "10200",      #Ungdomsuddannelse
+    "10400",      #Videregående uddannelse
+    "10500",      #Undervisning uden for niveau
+    "11   ",      #RESTAURANTER OG HOTELLER
+    "11111",      #Restauranter, cafeer mv
+    "11112",      #Fastfood, takeaway
+    "11120",      #Kantiner
+    "11201",      #Hoteller, moteller, kroer ol
+    "11202",      #Feriecentre, campingpladser, vandrerhjem ol
+    "11203",      #Anden indlogering
+    "12   ",      #ANDRE VARER OG TJENESTER
+    "12111",      #Frisør til mænd og børn 0-13 år
+    "12112",      #Frisør til kvinder
+    "12113",      #Behandlinger inden for personlig pleje
+    "12121",      #Elektriske apparater til personlig pleje
+    "12122",      #Reparation af elektriske apparater til personlig pleje
+    "12131",      #Ikke-elektrisk udstyr
+    "12132",      #Artikler til personlig hygiejne ol
+    "12200",      #Prostitution
+    "12311",      #Smykker
+    "12312",      #Ure
+    "12313",      #Reparation af smykker og ure
+    "12321",      #Tasker, rygsække ol
+    "12322",      #Babyartikler
+    "12323",      #Reparation af andre personlige effekter
+    "12329",      #Andre personlige effekter
+    "12401",      #Børnepasning
+    "12403",      #Madudbringning til pensionister
+    "12510",      #Livsforsikringer
+    "12520",      #Forsikring i forbindelse med boligen
+    "12532",      #Privat sygeforsikring
+    "12541",      #Transportforsikring
+    "12542",      #Rejseforsikring
+    "12550",      #Anden forsikring
+    "12621",      #Gebyr til banker mv
+    "12622",      #Revisor og anden finansiel rådgivning
+    "12701",      #Administrationsgebyrer
+    "12702",      #Juridiske tjenesteydelser
+    "12703",      #Begravelsestjenester
+    "12704")      #Andre gebyrer og tjenester
 
+
+
+# LAVER NYT DATASÆT ------------------------------------
 df_ny <- data.frame(decil=c("Samlet",1,2,3,4,5,6,7,8,9,10))
-
-df_ny <- df_ny %>% 
-  mutate(fisk = df %>% 
-  select(starts_with("113")) %>% 
-  rowSums())
-
-df_ny <- df_ny %>% 
-  mutate(ost2 = df %>% 
-  select(starts_with("114")) %>% 
-  rowSums())
-
-df_ny <- df_ny %>%
-  mutate(ost= 
-  df %>%
-  select(starts_with("114")) %>%
-  rowSums()
-  )
-
+      
 for (i in varegrupper) {
   nam <- paste0("A_", i)
-  df %>%
-     mutate( ost = df %>% 
-     select(starts_with("113")) %>%  
-     rowSums())
+  df_ny <- df_ny %>%
+    mutate(!!sym(nam) := 
+      df %>% 
+        select(starts_with(toString(i))) %>%  
+        rowSums())
 }
 
-for (lag_size in c(1, 5, 10, 15, 20)) {
-  
-  new_col_name <- paste0("lag_result_", lag_size)
-  
-  grouped_data <- grouped_data %>% 
-    mutate(!!sym(new_col_name) := lag(Result, n = lag_size, default = NA))
-}
 
-require(data.table)
-DT <- data.table(x=1:4, skill_a=c(0,1,0,0), skill_b=c(0,1,1,0), skill_c=c(0,1,1,1))
-
-
-DT %>% mutate(count = DT %>% select(starts_with("skill_")) %>% rowSums())
-
-
-library(tidyverse)
-
-df <- tibble(
-  column1 = rnorm(100),
-  column2 = rnorm(100),
-  column3 = rnorm(100),
-  column4 = rnorm(100),
-  column5 = rnorm(100),
-  data1 = rnorm(100),
-  data2 = rnorm(100)
-)
-
-df2 <- df %>%
-  mutate_at(. , vars(starts_with("column")), funs(prod1 = . * data1, prod2 = . * data2))
 
